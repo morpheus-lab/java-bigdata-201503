38db8120585235853f14ba13a3419189087da023
Heejin Cha <heejincha1219@gmail.com>
2015-04-24 15:37:05 +0900

    redis logout

--- redis-app/src/com/bitacademy/nosql/redis/twitter/AuthUtil.java
+++ redis-app/src/com/bitacademy/nosql/redis/twitter/AuthUtil.java
@@ -31,37 +31,80 @@ public class AuthUtil implements ServletContextListener {
 		
 	}
 	
-	public static void login(String userName, String password,
-			HttpServletRequest req, HttpServletResponse res) {
+	public static boolean login(String userName, String password,
+			HttpServletRequest request, HttpServletResponse response) {
+		boolean result = false;
+		
 		JedisPool pool =  new JedisPool(new JedisPoolConfig(),
 				REDIS_HOST, REDIS_PORT);
 		try (Jedis jedis = pool.getResource()) {
 			String userId = jedis.hget("user_info", userName + ":user_id");
 			String storedPw = jedis.hget("user_info", userId + ":password");
 			
-			if (storedPw.equals(password)) {
+			if (storedPw != null && storedPw.equals(sha256Digest(password))) {
 				// 새로운 token 발행
-				String newAuthToken = UUID.randomUUID().toString();
+				String newAuthToken = UUID.randomUUID().toString().replaceAll("\\-", "");
 				// token을 DB에 저장
 				jedis.hset("user_info", userId + ":auth", newAuthToken);
 				jedis.hset("user_info", newAuthToken + ":user_id", userId);
 				// token을 쿠키 저장
-				storeCookie("auth", newAuthToken, res, 60 * 60 * 24);
+				storeCookie("auth", newAuthToken, response, 60 * 60 * 24);
 				// request attribute로 user_id 저장
-				req.getSession().setAttribute("user_id", userId);
-				req.getSession().setAttribute("username", userName);
+				request.getSession().setAttribute("user_id", userId);
+				request.getSession().setAttribute("username", userName);
+				
+				result = true;
 			}
+		}
+		pool.destroy();
+		
+		return result;
+	}
+	
+	public static void logout(String userId, HttpServletRequest request,
+			HttpServletResponse response) {
+		// 세션 정보 삭제
+		request.getSession().removeAttribute("user_id");
+		request.getSession().removeAttribute("username");
+		
+		// 쿠키에 저장되어 있던 auth
+		String cookieAuth = null;
+		Cookie[] cookies = request.getCookies();
+		if (cookies != null) {
+			for (Cookie c : cookies) {
+				if ("auth".equals(c.getName())) {
+					cookieAuth = c.getValue();
+					break;
+				}
+			}
+		}
+		
+		// 쿠키 정보 삭제
+		storeCookie("auth", "", response, 0);
+		
+		// Redis DB 정보 삭제
+		JedisPool pool =  new JedisPool(new JedisPoolConfig(),
+				REDIS_HOST, REDIS_PORT);
+		try (Jedis jedis = pool.getResource()) {
+			if (cookieAuth != null) {
+				jedis.hdel("user_info", cookieAuth + ":user_id");
+			}
+			
+			String auth = jedis.hget("user_info", userId + ":auth");
+			jedis.hdel("user_info", userId + ":auth");
+			jedis.hdel("user_info", auth + ":user_id");
 		}
 		pool.destroy();
 	}
 	
 	public static void storeCookie(String cookieName, String cookieValue,
-			HttpServletResponse res, int expiry) {
+			HttpServletResponse response, int expiry) {
 		Cookie cookie = new Cookie(cookieName, cookieValue);
 		cookie.setMaxAge(expiry);
-		res.addCookie(cookie);
+		response.addCookie(cookie);
 	}
 	
+	/*
 	public static void checkAuth(HttpServletRequest req) {
 		Cookie authCookie = null;
 		Cookie[] cookies = req.getCookies();
@@ -88,6 +131,7 @@ public class AuthUtil implements ServletContextListener {
 		}
 		pool.destroy();
 	}
+	*/
 	
 	public static String sha256Digest(String str) {
 		String sha = "";
